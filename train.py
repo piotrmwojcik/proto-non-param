e4e985634a62dcaad41539d234287ead844df1ea
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2026-03-04 14:23:09 +0100

    Fixed dataset

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from pathlib import Path
 import argparse
 import wandb
 import numpy as np
-from pnp import CLIPPatch16Backbone
+from modeling.pnp import CLIPPatch16Backbone
 import torchvision
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
